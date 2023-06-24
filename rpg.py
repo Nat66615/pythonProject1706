7fd6a3bb29860819a661bbdb366404a0fb36f666
Nat66615 <nat.ja@mail.ru>
2023-06-24 20:42:11 +0500

    rpg +wrappers

--- rpg.py
+++ rpg.py
@@ -2,7 +2,7 @@ import random
 
 
 class Character:
-    def __init__(self, name, health, attack ):
+    def __init__(self, name, health, attack):
         self.name = name
         self.health = health
         self.attack = attack
@@ -33,15 +33,70 @@ class Player(Character):
         else:
             print('нет зелья')
 
+
 class Enemy(Character):
     pass
+
+
+def print_turn(func):
+    def wrapper(*args, **kwargs):
+        print('===== Начало хода =====')
+        result = func(*args, **kwargs)
+        print('===== Конец функции =====')
+        return result
+
+    return wrapper
+
+
+def random_damage(func):
+    def wrapper(*args, **kwargs):
+        damage = random.randint(1, 10)
+        print(f'урон: {damage}')
+        kwargs['damage'] = damage
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def check_alive(func):
+    def wrapper(*args, **kwargs):
+        if args[0].is_alive():
+            return func(*args, **kwargs)
+        else:
+            print('Персонаж мертв!')
+
+    return wrapper
+
+
+def print_action(func):
+    def wrapper(*args, **kwargs):
+        action_name = func.__name__.replace('_', ' ').capitalize()
+        print(f'{args[0].name} выбрал действие: {action_name}')
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@print_turn
+@print_action
+@check_alive
+@random_damage
+def attack_target(attacker, target, damage):
+    target.take_damage(damage)
+
+
+@print_turn
+@print_action
+@check_alive
+def use_portions(player):
+    player.use_portions()
+
 
 player = Player('Герой', 100, 20)
 enemy = Enemy('Монстр', 50, 10)
 
 print('Добро пожаловать в консольную RPG')
 
-#Основной цикл игры
+# Основной цикл игры
 while player.is_alive() and enemy.is_alive():
     print(f'{player.name}: Здоровье - {player.health}'
           f'Лечебное зелье - {player.portions}')
@@ -51,7 +106,7 @@ while player.is_alive() and enemy.is_alive():
                    '2 - Использовать зелье\n'
                    '(1-2):')
     if choice == '1':
-        player.attack_target(enemy)
+        attack_target(player, enemy)
         if not enemy.is_alive():
             print(f'{enemy.name} повержен')
     elif choice == 2:
@@ -60,7 +115,7 @@ while player.is_alive() and enemy.is_alive():
         print('выбор не верен')
 
     if enemy.is_alive():
-        enemy.attack_target(player)
+        attack_target(enemy, player)
         if not player.is_alive():
             print(f'{player.name} погиб!')
 
@@ -69,4 +124,3 @@ if player.is_alive():
     print('Вы победили')
 else:
     print('Вы проиграли!')
-
